ed81189a9f767000e9813c1f75c89315597a0077
shivambarot333 <shivambarot8260@gmail.com>
2025-01-10 13:00:04 +0530

    Add files via upload

---
+++ 24bcl103.py
@@ -0,0 +1,20 @@
+
+# // Add two number...
+a= float(input("enter a number:"))
+b=float(input("enter the another number:"))
+c=a+b
+print(c)
+print(a,b,c)
+print("answer =",c)
+print(a"+",b,'=',c)
+print(a,b,c,end="shivam")
+print(a,b,c,sep="#")
+
+
+
+
+print(type(a))
+print(type(b))
+print(id(a))
+print(id(b))
+
