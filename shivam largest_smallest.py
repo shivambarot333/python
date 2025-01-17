2cf7a3a673a9be3d64b92a58ffbd7ed256cde849
shivambarot333 <shivambarot8260@gmail.com>
2025-01-17 12:30:50 +0530

    Add files via upload

---
+++ shivam largest_smallest.py
@@ -0,0 +1,11 @@
+def largest_smallest():
+    number =int(input("enter the number :"))
+    if number >0:
+         print("largest")
+        
+    else:
+           print ("smallest")     
+
+largest_smallest()
+largest_smallest()
+largest_smallest()
