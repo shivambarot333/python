71f03ee2b16dfe5e4d7a4d1684b75a35570e2cf6
shivambarot333 <shivambarot8260@gmail.com>
2025-01-24 12:30:51 +0530

    Add files via upload

---
+++ vowels.py
@@ -0,0 +1,6 @@
+str=input("enter the your string")
+count=0
+for i in str:
+    if i=='a'or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='u':
+       count= count+1
+    print ("number of vowels are :",count)
