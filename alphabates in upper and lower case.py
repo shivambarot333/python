38c6452dabef1e0f9740f395779a8380794ffe19
shivambarot333 <shivambarot8260@gmail.com>
2025-01-31 12:04:21 +0530

    Add files via upload

---
+++ alphabates in upper and lower case.py
@@ -0,0 +1,9 @@
+
+print("upper case letters:")
+for char in range(65,91):
+       print(chr(char),end ="")
+    
+       print ("lower case letterts:")
+for char in range(97,123):
+    
+       print(chr(char),end="")
