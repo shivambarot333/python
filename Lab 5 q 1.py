9c1ac20f4050f39a387e4a8b01c3c98641b00e97
shivambarot333 <shivambarot8260@gmail.com>
2025-02-07 11:35:29 +0530

    Update Lab 5 q 1.py

--- Lab 5 q 1.py
+++ Lab 5 q 1.py
@@ -1,16 +1,16 @@
 import random
 
-# Generate lists
+
 odd_list = random.sample(range(1, 100, 2), 5)
 even_list = random.sample(range(0, 100, 2), 4)
 
 print("Odd list:", odd_list)
 print("Even list:", even_list)
 
-# Replace third element and flatten
+
 odd_list[2:3] = even_list
 print("Modified list:", odd_list)
 
-# Sort and print
+
 odd_list.sort()
 print("Sorted list:", odd_list)
