e35a1f2e80e8b002f1b828ebad7fb3a6f89b70b9
shivambarot333 <shivambarot8260@gmail.com>
2025-03-28 11:24:35 +0530

    Add files via upload

---
+++ ssssss.py
@@ -0,0 +1,18 @@
+def fun():
+    print("inside fun ()")
+def disp():
+    print("inside disp ()")
+def msg():
+    print("inside msg ()")
+
+l = [fun(), msg(),disp()]
+
+
+
+
+
+
+
+
+#for f in l :
+ #   f()
