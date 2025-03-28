a0eebbe68754b5927e5ab782e9112426bbd512ff
shivambarot333 <shivambarot8260@gmail.com>
2025-03-28 11:35:47 +0530

    Add files via upload

---
+++ lambda 2 .py
@@ -0,0 +1,20 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+====================== RESTART: C:/Users/lab/ssssss.py ======================
+>>> 
+====================== RESTART: C:/Users/lab/ssssss.py ======================
+>>> 
+====================== RESTART: C:/Users/lab/ssssss.py ======================
+inside fun ()
+inside msg ()
+inside disp ()
+>>> 
+====================== RESTART: C:/Users/lab/ssssss.py ======================
+inside fun ()
+inside msg ()
+inside disp ()
+>>> 
+======================= RESTART: C:/Users/lab/2222 .py =======================
+<map object at 0x02E560D0>
+>>> 
